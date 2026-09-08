9bf631e3c3164e849b5f980019aab1135c3ab7bb
IT Jason <13651470600@163.com>
2026-09-08 11:44:27 +0800

    hide settings popup menu

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -220,7 +220,6 @@ class _DesktopHomePageState extends State<DesktopHomePage>
                                   ?.color
                                   ?.withOpacity(0.5)),
                         ).marginOnly(top: 5),
-                        buildPopupMenu(context)
                       ],
                     ),
                   ),
